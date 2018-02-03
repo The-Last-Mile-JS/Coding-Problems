df922d2c8215563bc71e1486d444c8130cc30a92
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2018-02-02 20:34:34 -0800

    unit testing

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var test = require('tape').test; // assign the tape library to the variable "test"
 const testFolder = '../submission/';
 const fs = require('fs');
+var leaderboard = new Array();
 fs.readdirSync(testFolder).forEach(file => { //go through all the files in the folder
   var parts = file.split('.');
   var ext = parts[parts.length - 1]; //get the extension and name
@@ -19,6 +20,23 @@ fs.readdirSync(testFolder).forEach(file => { //go through all the files in the f
     for (i = 1; i <= 1e7; i++) //run the function 10 million times
       square(3);
     var end = new Date().getTime();
-    console.log(end - start) //calculate the complexity by subtracting start time from end time
+    leaderboard.push([end - start, name]) //calculate the complexity by subtracting start time from end time
   }
 })
+
+leaderboard.sort(sortFunction);
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
+
+for(i = 0; i < leaderboard.length; i++){
+    console.log(leaderboard[i][1] + " score is " + leaderboard[i][0]);
+}
+
+console.log()
